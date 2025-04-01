ebcbafb4a162e99385cebd3c4ad3bbcb24d19d08
Deepak Choudhary <choudharydeepak990@gmail.com>
2025-04-01 22:47:36 +0530

    remove: Duplicate Code removed

--- src/presentation/ImageFeedItemRenderer.kt
+++
@@ -1,9 +0,0 @@
-package presentation
-
-import domain.model.FeedItem
-
-class ImageFeedItemRenderer: FeedItemRenderer {
-    override fun render(feedItem: FeedItem): FeedView {
-        TODO("Not yet implemented")
-    }
-}
